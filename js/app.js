7c6fb878d0bb4fd9578da26a01bb33310be44620
bywillsilva <raniwillere@gmail.com>
2023-05-31 12:57:06 -0300

    Atualizando menu mobile

---
+++ js/app.js
@@ -0,0 +1,21 @@
+const menu_icon = document.getElementById("menu-icon");
+const close_icon = document.getElementById("close-icon");
+const menu_mobile = document.getElementById("menu-mobile");
+const body = document.querySelector("body");
+menu_icon.addEventListener("click", () => {
+    menu_mobile.style.display = "flex";
+    body.style.overflow = "hidden";
+});
+
+close_icon.addEventListener("click", () => {
+    menu_mobile.style.display = "none";
+    body.style.overflow = "auto";
+});
+
+let mobile_li = document.getElementsByClassName("mobile-li");
+for (let index = 0; index < mobile_li.length; index++) {
+    mobile_li[index].addEventListener("click", () => {
+        menu_mobile.style.display = "none";
+        body.style.overflow = "auto";
+    });
+};
