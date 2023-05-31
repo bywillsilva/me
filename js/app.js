86c2e7d6a8bf1a84909d2516b3b1d61b589569ba
bywillsilva <raniwillere@gmail.com>
2023-05-31 13:14:22 -0300

    Atualizando interação de menu mobile

--- js/app.js
+++ js/app.js
@@ -3,19 +3,22 @@ const close_icon = document.getElementById("close-icon");
 const menu_mobile = document.getElementById("menu-mobile");
 const body = document.querySelector("body");
 menu_icon.addEventListener("click", () => {
-    menu_mobile.style.display = "flex";
+    menu_mobile.classList.add("animate-menu");
+    menu_mobile.style.right = "0";
     body.style.overflow = "hidden";
 });
 
 close_icon.addEventListener("click", () => {
-    menu_mobile.style.display = "none";
+    menu_mobile.classList.remove("animate-menu");
+    menu_mobile.style.right = "-380px";
     body.style.overflow = "auto";
 });
 
 let mobile_li = document.getElementsByClassName("mobile-li");
 for (let index = 0; index < mobile_li.length; index++) {
     mobile_li[index].addEventListener("click", () => {
-        menu_mobile.style.display = "none";
+        menu_mobile.classList.remove("animate-menu");
+        menu_mobile.style.right = "-380px";
         body.style.overflow = "auto";
     });
 };
